b9f08e02fd4d35f862cdb9a052cd869c0f2913a5
bifurcated <litvinenko-maks@inbox.ru>
2024-01-13 05:31:42 +0300

    Change mapping url and use Optional

--- src/main/java/com/bifurcated/wallet/controller/WalletController.java
+++ src/main/java/com/bifurcated/wallet/controller/WalletController.java
@@ -1,17 +1,14 @@
 package com.bifurcated.wallet.controller;
 
-import com.bifurcated.wallet.data.Wallet;
 import com.bifurcated.wallet.errors.UnsupportedOperationTypeError;
-import com.bifurcated.wallet.service.WalletService;
 import com.bifurcated.wallet.operation.OperationType;
+import com.bifurcated.wallet.service.WalletService;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -34,13 +31,13 @@ public class WalletController {
             String operationType,
             Float amount
     ){}
-    @PostMapping
+    @PostMapping("/wallet")
     public WalletResponse wallet(@RequestBody WalletRequest request) {
-        String operationType = request.operationType().toLowerCase();
-        if (operations.containsKey(operationType)) {
-            Wallet wallet = operations.get(operationType).handle(request.walletId(), request.amount());
-            return new WalletResponse(wallet.getId(), wallet.getAmount());
-        }
-        throw new UnsupportedOperationTypeError();
+        var operationType = request.operationType().toLowerCase();
+        var wallet = Optional.ofNullable(operations.get(operationType))
+                .orElseThrow(UnsupportedOperationTypeError::new)
+                .handle(request.walletId(), request.amount());
+
+        return new WalletResponse(wallet.getId(), wallet.getAmount());
     }
 }
